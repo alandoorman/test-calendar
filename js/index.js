dd49cbd67d0faa9a249e3e69abe27245105a1d46
Alan Doorman <35386212+alandoorman@users.noreply.github.com>
2018-01-13 18:02:31 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function mayo(){
+    document.getElementById("mov").style.transform = "translateX(0%)";
+    
+    document.getElementById("mayo").style.background = "#00b5af";
+    
+    document.getElementById("junio").style.background = "#ff4c4c";
+
+    document.getElementById("julio").style.background = "#ff4c4c";
+}
+
+function junio(){
+    document.getElementById("mov").style.transform = "translateX(-33%)";
+
+    document.getElementById("mayo").style.background = "#ff4c4c";
+    
+    document.getElementById("junio").style.background = "#00b5af";
+    
+    document.getElementById("julio").style.background = "#ff4c4c";
+    
+}
+
+function julio(){
+    document.getElementById("mov").style.transform = "translateX(-66%)";
+    
+    document.getElementById("mayo").style.background = "#ff4c4c";
+    
+        document.getElementById("junio").style.background = "#ff4c4c";
+    
+    document.getElementById("julio").style.background = "#00b5af";
+}
